0da0c2b67adfaedebf6f99662753aa7d90dd1b9e
ushabh-jain <ushabhjain07@gmail.com>
2024-11-10 18:51:58 +0530

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Blogs from "./pages/Blogs";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="App">
-        <h1>BLOGZz</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import {} from '@mui/material'
+import React, { useState } from "react";
+import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab } from "@mui/material";
+import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
 
-const header = () => {
+const Header = () => {
+    const isLogin = useSelector(state => state.isLogin)
+    const [value,setValue] = useState();
+    
   return (
-    <div>
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h4">BLOGZ</Typography>
+           {isLogin && (
+              <Box display={'flex'} marginleft="auto" marginRight={'auto'}>
+              <Tabs textColor="inherit" value={value} onChange={(e,val)=> setValue(val)}>
 
-    </div>
-  )
-}
+                <Tab  sx={{ margin: 1, color: "white" }} label= "Blogs" LinkComponent={Link} to ="/blog" />
+                <Tab  sx={{ margin: 1, color: "white" }} label= "My Blogs" LinkComponent={Link} to ="/my-blog" />
 
-export default header
+              </Tabs>
+          </Box>
+           )}
+          <Box display={"flex"} marginLeft="auto">
+            {!isLogin && (
+              <>
+              <Button
+              sx={{ margin: 1, color: "white" }}
+              LinkComponent={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              sx={{ margin: 1, color: "white" }}
+              LinkComponent={Link}
+              to="/register"
+            >
+              Register
+            </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button sx={{ margin: 1, color: "white" }}>Log Out</Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
