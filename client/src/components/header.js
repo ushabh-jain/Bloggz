d7082f21b5c30417b5f5e1462e0514602eb09f0f
ushabh-jain <ushabhjain07@gmail.com>
2024-11-14 20:23:11 +0530

    sixth commit

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
 import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab } from "@mui/material";
-import { Link } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import { Link, useNavigate } from "react-router-dom";
+import {useSelector,useDispatch} from 'react-redux'
+import { authActions } from "../redux/store";
+import toast from "react-hot-toast";
 
 const Header = () => {
-    const isLogin = useSelector(state => state.isLogin)
+    const isLogin = useSelector(state => state.isLogin);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const [value,setValue] = useState();
+
+    const handleLogout = () =>{
+      try {
+         dispatch(authActions.logout());
+         toast.success("Logout Successfully");
+         navigate("/login");
+         localStorage.clear();
+
+
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+
     
   return (
     <>
@@ -42,7 +61,7 @@ const Header = () => {
               </>
             )}
             {isLogin && (
-              <Button sx={{ margin: 1, color: "white" }}>Log Out</Button>
+              <Button onClick={handleLogout} sx={{ margin: 1, color: "white" }}>Log Out</Button>
             )}
           </Box>
         </Toolbar>
