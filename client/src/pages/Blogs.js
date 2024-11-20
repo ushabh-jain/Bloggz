c6db44b6f87a1847d77ee82d47e28eea0e0c5947
ushabh-jain <ushabhjain07@gmail.com>
2024-11-20 09:14:59 +0530

    seventh commit

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Blogs = () => {
+  const[blogs,setBlogs] = useState([]);
+
+  // getblogs
+  const getAllBlogs = async() => {
+    try {
+      const{data} = await axios.get('/api/v1/blog/all-blog')
+      if(data?.success){
+        setBlogs(data?.blogs);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(()=>{
+    getAllBlogs();
+  },[]);
   return (
     <div>Blogs</div>
   )
